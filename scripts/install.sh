e1c746af605957d7dc1368ad496296e219160963
Anun Panya <aspirinenun@gmail.com>
2019-06-09 16:35:21 +0700

    update

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,5 +3,10 @@
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl --kubeconfig ./scripts/config version
-kubectl apply -f deploy/kubernetes/complete-demo.yaml
+
+mkdir -p $(HOME)/.kube
+mv ./scripts/config $(HOME)/.kube/config
+kubectl version
+kubectl get nodes
+
+#kubectl apply -f deploy/kubernetes/complete-demo.yaml
